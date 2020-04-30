55622bb989458a08dff2ae7a57741d0e862aa0c2
Segura <segura-deluna@mail.ru>
2020-04-30 18:07:02 +0300

    added header with adaptive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-const userIcon = document.querySelector('.user-header__icon');
+const userIcon = document.querySelector('.user-header__icon'),
+	burger = document.querySelector('.menu-burger'),
+	menuBody = document.querySelector('.menu-body');
+
 
 userIcon.addEventListener('click', (e) => {
 	const userMenu = document.querySelector('.action-header__menu');
 	userMenu.classList.toggle('active');
+});
+
+burger.addEventListener('click', (e) => {
+	burger.classList.toggle('active');
+	menuBody.classList.toggle('active');
 });
