c3c544abfd6e3004f77951c2b84c3e702cb5d661
Segura <segura-deluna@mail.ru>
2020-05-03 20:47:38 +0300

    added slider, fixed header

--- js/main.js
+++ js/main.js
@@ -2,13 +2,26 @@ const userIcon = document.querySelector('.user-header__icon'),
 	burger = document.querySelector('.menu-burger'),
 	menuBody = document.querySelector('.menu-body');
 
-
+// Icon region user
 userIcon.addEventListener('click', (e) => {
 	const userMenu = document.querySelector('.action-header__menu');
 	userMenu.classList.toggle('active');
 });
-
+// Burger-menu
 burger.addEventListener('click', (e) => {
 	burger.classList.toggle('active');
 	menuBody.classList.toggle('active');
+});
+
+//* SLIDER =================================
+var mySwiper = new Swiper('.swiper-container', {
+	// Optional parameters
+	loop: true,
+	observer: true,
+	observeParents: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 });
