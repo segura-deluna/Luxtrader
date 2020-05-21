ace29d532c53a216d132d554d20b2a6e9094ea77
Segura <segura-deluna@mail.ru>
2020-05-22 01:40:06 +0300

    added lots slider | добавлен слайдер лотов

--- js/main.js
+++ js/main.js
@@ -14,15 +14,35 @@ burger.addEventListener('click', (e) => {
 	menuBody.classList.toggle('active');
 });
 
-//* SLIDER =================================
-var mySwiper = new Swiper('.swiper-container', {
+
+
+//* SLIDERS =================================
+
+const mainSlider = new Swiper('.main-slider__container', {
 	// Optional parameters
 	loop: true,
 	observer: true,
 	observeParents: true,
+	slidesPerView: 1,
+	speed: 800,
 	// Navigation arrows
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
+	},
+});
+
+const lotSlider = new Swiper('.lots__slider', {
+	// Optional parameters
+	loop: true,
+	observer: true,
+	observeParents: true,
+	slidesPerView: 3,
+	speed: 800,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.control-lots__arrow-next',
+		prevEl: '.control-lots__arrow-prev',
 	},
 });
